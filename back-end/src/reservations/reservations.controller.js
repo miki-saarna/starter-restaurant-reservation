f2f6eb2a07fb94d7196c369ae506024ebff56b7f
Mikito Saarna <mikitosaarna@gmail.com>
2021-12-24 17:38:07 -0600

    Added timezone equations to set reservations at current time based on timezone/location

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -14,8 +14,45 @@ async function list(req, res) {
   }
 }
 
-async function create(req, res) {
+async function create(req, res, next) {
   const { data } = req.body;
+  
+  const dateArray = data.reservation_date.split('-')
+  // new Date() format requires month index, which is 0 indexed
+  dateArray[1] -= 1;
+  const timeArray = data.reservation_time.split(':')
+  
+  // get timezone offset in ms
+  const presentDateUTC = new Date();
+  const timezoneOffset = presentDateUTC.getTimezoneOffset() * 60000;
+
+
+  const reservationDateUTC = new Date(...dateArray, ...timeArray);
+  const reservationDate = new Date(reservationDateUTC.getTime() - timezoneOffset)
+
+
+  // use getTime() method to convert to ms, which will allow us to subtract the timezoneOffset
+  const presentDate = new Date(presentDateUTC.getTime() - timezoneOffset);
+  // validation if reservation date is in the past
+
+  if (reservationDate - presentDate < 0) {
+    return next({ status: 400, message: `Reservation date cannot be for a past date.`})
+  }
+
+  // validation if reservation is on a tuesday
+  if (reservationDate.getDay() === 2) {
+    return next({ status: 400, message: `Our restaurant is closed on Tuesday to allow the employees to rest and enjoy their day.`})
+  }
+
+  // validation if reservation is during open hours
+  const reservationTime = data.reservation_time;  
+  if (reservationTime < '10:30:00') {
+    return next({ status: 400, message: `The restaurant does not open until 10:30 AM.`})
+  }
+
+  if (reservationTime > '21:30:00') {
+    return next({ status: 400, message: `Please make a reservation at least 1 hour prior to closing. The restaurant closes at 10:30 PM.`})
+  }
   newReservation = await service.create(data);
   res.status(201).json({ data: newReservation })
 }
