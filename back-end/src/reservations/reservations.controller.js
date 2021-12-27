395902afceb7c67b03184b13a8f687882984f0d2
Mikito Saarna <mikitosaarna@gmail.com>
2021-12-26 22:16:19 -0600

    New reservation form created with post method to database

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -16,35 +16,39 @@ async function list(req, res) {
 
 async function create(req, res, next) {
   const { data } = req.body;
-  
+  // console.log(data)
+  const presentDateUTC = new Date();
+  // get timezone offset in ms
+  const timezoneOffset = presentDateUTC.getTimezoneOffset() * 60000;
+  // get present date/time with timezone consideration
+  // use getTime() method to convert to ms, which will allow us to subtract the timezoneOffset
+  const presentDate = new Date(presentDateUTC.getTime() - timezoneOffset);
+
+
+  // separate year, month, day
   const dateArray = data.reservation_date.split('-')
   // new Date() format requires month index, which is 0 indexed
   dateArray[1] -= 1;
+  // separate hour, minute, second
   const timeArray = data.reservation_time.split(':')
-  
-  // get timezone offset in ms
-  const presentDateUTC = new Date();
-  const timezoneOffset = presentDateUTC.getTimezoneOffset() * 60000;
-
 
   const reservationDateUTC = new Date(...dateArray, ...timeArray);
+  // get reservation date/time with timezone consideration
+  // use getTime() method to convert to ms, which will allow us to subtract the timezoneOffset
   const reservationDate = new Date(reservationDateUTC.getTime() - timezoneOffset)
 
-
-  // use getTime() method to convert to ms, which will allow us to subtract the timezoneOffset
-  const presentDate = new Date(presentDateUTC.getTime() - timezoneOffset);
+  
   // validation if reservation date is in the past
-
   if (reservationDate - presentDate < 0) {
-    return next({ status: 400, message: `Reservation date cannot be for a past date.`})
+    return next({ status: 400, message: `Reservation date and time cannot be for a past date.`})
   }
 
   // validation if reservation is on a tuesday
   if (reservationDate.getDay() === 2) {
-    return next({ status: 400, message: `Our restaurant is closed on Tuesday to allow the employees to rest and enjoy their day.`})
+    return next({ status: 400, message: `Our restaurant is closed on Tuesday to allow the employees time to rest and enjoy their day.`})
   }
 
-  // validation if reservation is during open hours
+  // validation if reservation is not during open hours
   const reservationTime = data.reservation_time;  
   if (reservationTime < '10:30:00') {
     return next({ status: 400, message: `The restaurant does not open until 10:30 AM.`})
@@ -53,6 +57,7 @@ async function create(req, res, next) {
   if (reservationTime > '21:30:00') {
     return next({ status: 400, message: `Please make a reservation at least 1 hour prior to closing. The restaurant closes at 10:30 PM.`})
   }
+  
   newReservation = await service.create(data);
   res.status(201).json({ data: newReservation })
 }
